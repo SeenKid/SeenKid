a28442b16549f2f251c09ba7c86a308295953955
SeenKid <30565751+SeenKid@users.noreply.github.com>
2022-06-05 19:49:53 +0200

    Add files via upload

---
+++ Hello World/main.cs
@@ -0,0 +1,11 @@
+// Hello world program in C#
+using System;
+
+namespace HelloWorld {
+	class Mrbluebird {
+		static void Main(string[] args) {
+			Console.WriteLine("Hello World!");
+			Console.ReadKey();
+		}
+	}
+}
---
+++ Hello World/main.java
@@ -0,0 +1,9 @@
+// Hello world program in java
+
+// Creating Class HelloWorld
+class HelloWorld {
+    // Public static void
+    public static void main(String[] args) {
+        System.out.println("Hello, World!"); 
+    }
+}
