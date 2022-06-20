fc056c664f886719b7d1a7dded7e8dfb2af7e175
SeenKid <30565751+SeenKid@users.noreply.github.com>
2022-06-20 11:03:54 +0200

    Update main.java

--- Hello World/main.java
+++ Hello World/main.java
@@ -1,8 +1,6 @@
 // Hello world program in java
 
-// Creating Class HelloWorld
 class HelloWorld {
-    // Public static void
     public static void main(String[] args) {
         System.out.println("Hello, World!"); 
     }
